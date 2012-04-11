a2fc4212014e33dbe0375551e7cd88e9e84c8440
Benjamin Eberlei <kontakt@beberlei.de>
2012-04-11 15:33:29 +0200

    Add missing QueryFactory details passing settings to Quer

--- Classes/QueryFactory.php
+++ Classes/QueryFactory.php
@@ -7,6 +7,16 @@ class Tx_Doctrine2_QueryFactory implements Tx_Extbase_Persistence_QueryFactoryIn
      */
     protected $manager;
 
+	/**
+	 * @var Tx_Extbase_Object_ObjectManagerInterface
+	 */
+	protected $objectManager;
+
+	/**
+	 * @var Tx_Extbase_Configuration_ConfigurationManagerInterface
+	 */
+	protected $configurationManager;
+
     /**
      * @param Tx_Doctrine2_Manager
      * @return void
@@ -16,6 +26,20 @@ class Tx_Doctrine2_QueryFactory implements Tx_Extbase_Persistence_QueryFactoryIn
         $this->manager = $manager;
     }
 
+	public function injectObjectManager(Tx_Extbase_Object_ObjectManagerInterface $objectManager)
+    {
+		$this->objectManager = $objectManager;
+	}
+
+	/**
+	 * @param Tx_Extbase_Configuration_ConfigurationManagerInterface $configurationManager
+	 * @return void
+	 */
+	public function injectConfigurationManager(Tx_Extbase_Configuration_ConfigurationManagerInterface $configurationManager)
+    {
+		$this->configurationManager = $configurationManager;
+	}
+
     /**
      * @return Tx_Doctrine2_Query
      */
@@ -23,6 +47,12 @@ class Tx_Doctrine2_QueryFactory implements Tx_Extbase_Persistence_QueryFactoryIn
     {
         $query = new Tx_Doctrine2_Query($className);
         $query->injectEntityManager($this->manager->getEntityManager());
+
+		$querySettings = $this->objectManager->create('Tx_Extbase_Persistence_QuerySettingsInterface');
+		$frameworkConfiguration = $this->configurationManager->getConfiguration(Tx_Extbase_Configuration_ConfigurationManagerInterface::CONFIGURATION_TYPE_FRAMEWORK);
+		$querySettings->setStoragePageIds(t3lib_div::intExplode(',', $frameworkConfiguration['persistence']['storagePid']));
+		$query->setQuerySettings($querySettings);
+
         return $query;
     }
 }
